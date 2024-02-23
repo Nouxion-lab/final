f57aca2725dc22533452c7e42ee295638f266556
Nouxion-lab <141471556+Nouxion-lab@users.noreply.github.com>
2024-02-23 16:56:44 +0700

    test2.py

--- test ceit sofware/test2.py
+++ test ceit sofware/test2.py
@@ -1,12 +1,11 @@
 
-result = 2**10
-
-result_str = result
-
-digit_sum = ''
+result = 2**1337
+result_str = str(result)
+digit_sum = 0
 
 for digit in result_str:
-    digit_sum += digit
+    digit_sum += int(digit)
 
 print(f'2^1337 = {result}')
 print(f'Sum of digits: {digit_sum}')
+
